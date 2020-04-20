c7c5d3fb7950d8cdd2aa0a69e31a253bcc7ed74e
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2020-04-20 09:19:44 +0500

    Fixed: ekb didn't support dead count

--- covid/ekb.py
+++ covid/ekb.py
@@ -5,6 +5,6 @@ def ekb():
     soup = url2soup('http://све.рф/стопвирус')
     table = soup.find('table', attrs={'class': 'region-table'})
     data = dict()
-    data['cases'], data['recovered'], data['tests'] = [int(td.text) for td in table.find('tr').find_all('td')]
+    data['cases'], data['recovered'], data['dead'], data['tests'] = [int(td.text) for td in table.find('tr').find_all('td')]
     for key, value in data.items():
         print("{0:12} {1}".format(key, value))
